902eae04aab8e5d8a8ab4aeba86c4eef598040d8
Hasan <H.Bilen@student.tudelft.nl>
2017-07-14 02:16:41 +0200

    Added ball center drawing, ball trajectory drawing and boost meter for all cars.

--- RLModding/Mods/Modules/TestClass.cpp
+++ RLModding/Mods/Modules/TestClass.cpp
@@ -39,13 +39,57 @@ void TestClass::onPostRender(SDK::UObject** object,SDK::UFunction* function,void
 
 	SDK::APlayerController* pPlayerController = (SDK::APlayerController*)Utils::GetInstanceOf(SDK::APlayerController::StaticClass());
 	SDK::ABall_TA* ball = (SDK::ABall_TA*)Utils::GetInstanceOf(SDK::ABall_TA::StaticClass());
+	SDK::AGameEvent_TA* gameEvent = (SDK::AGameEvent_TA*)Utils::GetInstanceOf(SDK::AGameEvent_TA::StaticClass());
 
-	if  (pPlayerController != NULL && ball != NULL) {
-		DrawManager::Instance()->BeginRendering();
-		SDK::FVector BallFixed = ball->Location;
-		BallFixed.Z += ball->Radius;
-		SDK::FVector vec = Vec::VecUtils::CalculateScreenCoordinate(BallFixed, pPlayerController);
-		DrawManager::Instance()->AddCircleFilled(ImVec2(vec.X,vec.Y),20, D3DCOLOR_ARGB(255, 127, 0, 127),32);
+	if (pPlayerController != NULL) {
+
+		// Draw ball location + circles each radius step below it until it reaches the ground
+		if (ball != NULL) {
+			DrawManager::Instance()->BeginRendering();
+
+			int real = ball->Location.Z;
+
+			for (int i = 0; i < 10; i++) {
+				ball->Location.Z = real - ball->Radius * i;
+
+				if (ball->Location.Z < 0)
+					continue;
+
+				SDK::FVector ball2DLocation = Vec::VecUtils::CalculateScreenCoordinate(ball->Location, pPlayerController);
+				
+				if (ball2DLocation.X >= 0 && ball2DLocation.Y >= 0 && ball2DLocation.X <= 900 && ball2DLocation.Y <= 1600) {
+					DrawManager::Instance()->AddCircleFilled(ImVec2(ball2DLocation.X, ball2DLocation.Y), 10, D3DCOLOR_ARGB(255, 127, 0, 127), 32);
+				}
+			}
+
+			ball->Location.Z = real;
+		}
+
+
+		if (gameEvent != NULL) {
+			for (int i = 0; i < gameEvent->Cars.Num(); i++) {
+				SDK::ACar_TA* car = gameEvent->Cars[i];
+				SDK::FVector car2DLocation = Vec::VecUtils::CalculateScreenCoordinate(car->Location, pPlayerController);
+				std::string boostText = "boost: " + std::to_string((int)(100.0f * car->BoostComponent->CurrentBoostAmount));
+				DrawManager::Instance()->AddText(ImVec2(car2DLocation.X - 2, car2DLocation.Y - 2), ImColor(255, 255, 255, 255), (text_flags)1, boostText.c_str());
+				DrawManager::Instance()->AddText(ImVec2(car2DLocation.X, car2DLocation.Y), ImColor(255, 0, 0, 255), (text_flags)1, boostText.c_str());
+			}
+
+			
+			// Ball trajectory (shitty)
+			SDK::FVector start = ball->GetTrajectoryStartLocation();
+			SDK::FVector vel = ball->GetTrajectoryStartVelocity();
+			SDK::FVector current = ball->GetTrajectoryStartLocation();
+			for (int i = 0; i < 10; i++) {
+
+				current.X = start.X + vel.X * i;
+				current.Y = start.Y + vel.Y * i;
+				current.Z = start.Z + vel.Z * i;
+
+				SDK::FVector trajectory2DLocation = Vec::VecUtils::CalculateScreenCoordinate(current, pPlayerController);
+				DrawManager::Instance()->AddLine(ImVec2(start.X, start.Y), ImVec2(trajectory2DLocation.X, trajectory2DLocation.Y), ImColor(0, 0, 255, 255), 2.0f);
+			}
+		}
 	}
 }
 void TestClass::onTCPConnectionBegin(SDK::UObject** object,SDK::UFunction* func,void* params) {}
