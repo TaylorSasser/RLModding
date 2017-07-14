a9c63f330a53ee4885ec049662cb1ae8aae72f9d
Hasan <H.Bilen@student.tudelft.nl>
2017-07-14 02:32:00 +0200

    Some drawing related visual improvements.

--- RLModding/Mods/Modules/TestClass.cpp
+++ RLModding/Mods/Modules/TestClass.cpp
@@ -43,13 +43,18 @@ void TestClass::onPostRender(SDK::UObject** object,SDK::UFunction* function,void
 
 	if (pPlayerController != NULL) {
 
+		DrawManager::Instance()->BeginRendering();
+
 		// Draw ball location + circles each radius step below it until it reaches the ground
 		if (ball != NULL) {
-			DrawManager::Instance()->BeginRendering();
+
+			SDK::FVector previousBall2DLocation = Vec::VecUtils::CalculateScreenCoordinate(ball->Location, pPlayerController);
+
+			// Draw dot in middle of ball
+			DrawManager::Instance()->AddCircleFilled(ImVec2(previousBall2DLocation.X, previousBall2DLocation.Y), 10, D3DCOLOR_ARGB(255, 127, 0, 127), 32);
 
 			int real = ball->Location.Z;
-
-			for (int i = 0; i < 10; i++) {
+			for (int i = 0; i < 30; i++) {
 				ball->Location.Z = real - ball->Radius * i;
 
 				if (ball->Location.Z < 0)
@@ -58,11 +63,28 @@ void TestClass::onPostRender(SDK::UObject** object,SDK::UFunction* function,void
 				SDK::FVector ball2DLocation = Vec::VecUtils::CalculateScreenCoordinate(ball->Location, pPlayerController);
 				
 				if (ball2DLocation.X >= 0 && ball2DLocation.Y >= 0 && ball2DLocation.X <= 900 && ball2DLocation.Y <= 1600) {
-					DrawManager::Instance()->AddCircleFilled(ImVec2(ball2DLocation.X, ball2DLocation.Y), 10, D3DCOLOR_ARGB(255, 127, 0, 127), 32);
+					//DrawManager::Instance()->AddCircleFilled(ImVec2(ball2DLocation.X, ball2DLocation.Y), 10, D3DCOLOR_ARGB(255, 127, 0, 127), 32);
+					DrawManager::Instance()->AddLine(ImVec2(previousBall2DLocation.X, previousBall2DLocation.Y), ImVec2(ball2DLocation.X, ball2DLocation.Y), ImColor(255, 127, 0, 127), 2.0f);
+					previousBall2DLocation = ball2DLocation;
 				}
 			}
 
 			ball->Location.Z = real;
+
+
+			// Ball trajectory (shitty)
+			SDK::FVector start = ball->GetTrajectoryStartLocation();
+			SDK::FVector vel = ball->GetTrajectoryStartVelocity();
+			SDK::FVector previousTrajectory2DLocation = Vec::VecUtils::CalculateScreenCoordinate(start, pPlayerController);
+			SDK::FVector current = ball->GetTrajectoryStartLocation();
+			for (int i = 0; i < 10; i++) {
+				current.X = start.X + vel.X * i;
+				current.Y = start.Y + vel.Y * i;
+				current.Z = start.Z + vel.Z * i;
+
+				SDK::FVector trajectory2DLocation = Vec::VecUtils::CalculateScreenCoordinate(current, pPlayerController);
+				DrawManager::Instance()->AddLine(ImVec2(previousTrajectory2DLocation.X, previousTrajectory2DLocation.Y), ImVec2(trajectory2DLocation.X, trajectory2DLocation.Y), ImColor(0, 0, 255, 255), 2.0f);
+			}
 		}
 
 
@@ -73,21 +95,6 @@ void TestClass::onPostRender(SDK::UObject** object,SDK::UFunction* function,void
 				std::string boostText = "boost: " + std::to_string((int)(100.0f * car->BoostComponent->CurrentBoostAmount));
 				DrawManager::Instance()->AddText(ImVec2(car2DLocation.X - 2, car2DLocation.Y - 2), ImColor(255, 255, 255, 255), (text_flags)1, boostText.c_str());
 				DrawManager::Instance()->AddText(ImVec2(car2DLocation.X, car2DLocation.Y), ImColor(255, 0, 0, 255), (text_flags)1, boostText.c_str());
-			}
-
-			
-			// Ball trajectory (shitty)
-			SDK::FVector start = ball->GetTrajectoryStartLocation();
-			SDK::FVector vel = ball->GetTrajectoryStartVelocity();
-			SDK::FVector current = ball->GetTrajectoryStartLocation();
-			for (int i = 0; i < 10; i++) {
-
-				current.X = start.X + vel.X * i;
-				current.Y = start.Y + vel.Y * i;
-				current.Z = start.Z + vel.Z * i;
-
-				SDK::FVector trajectory2DLocation = Vec::VecUtils::CalculateScreenCoordinate(current, pPlayerController);
-				DrawManager::Instance()->AddLine(ImVec2(start.X, start.Y), ImVec2(trajectory2DLocation.X, trajectory2DLocation.Y), ImColor(0, 0, 255, 255), 2.0f);
 			}
 		}
 	}
