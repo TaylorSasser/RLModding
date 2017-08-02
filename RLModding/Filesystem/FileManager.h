0591898a6b1dc0560dead5b22871446778452bae
Scott Mitchell <mitchsco@sonoma.edu>
2017-08-02 13:45:50 -0700

    Added essential boost libraries

--- RLModding/Filesystem/FileManager.h
+++ RLModding/Filesystem/FileManager.h
@@ -1,5 +1,11 @@
 #pragma once
 #include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/lexical_cast/bad_lexical_cast.hpp>
+#include <boost/lexical_cast.hpp>
+
+namespace pt = boost::property_tree;
 
 class FileManager {
 public:
