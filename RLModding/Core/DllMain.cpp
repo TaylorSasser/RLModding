e20102d4af56afe808cb8b1a90843c0ec39c32a3
Scott Mitchell <mitchsco@sonoma.edu>
2017-08-13 21:57:09 -0700

    Added Unfinished blacklist and fixed linker errors

--- RLModding/Core/DllMain.cpp
+++ RLModding/Core/DllMain.cpp
@@ -25,7 +25,7 @@ void onAttach(HMODULE hModule) {
 	Interfaces::DX9Handler().InitGUI();
 	Interfaces::KeyboardHandler().HookKeyboard();
 	Interfaces::FileHandler().Load();
-	Interfaces::Blacklist().Check();
+	//Interfaces::Blacklist().Check();
 }
 
 void onDetach(HMODULE hModule) {
