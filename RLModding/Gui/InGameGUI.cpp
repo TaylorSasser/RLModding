012b03bf607f916f78c70820a6269cb08df8a69a
Tim <timothyunrue@gmail.com>
2017-08-14 17:20:49 -0400

    Fixed drawing the mod menus

--- RLModding/Gui/InGameGUI.cpp
+++ RLModding/Gui/InGameGUI.cpp
@@ -21,21 +21,20 @@ void InGameGUI::Render() {
 
 	// Draw mouse cursor in game (since it is disabled);
 	ImGui::GetIO().MouseDrawCursor = true;
-	ImGui::GetStyle().Alpha = 50;
+	//ImGui::GetStyle().Alpha = 50;  // This made it so you couldn't see diddly peat
 
 	if (ImGui::BeginMainMenuBar()) {
 		if (ImGui::BeginMenu("Ball Mods")) {
 			for (auto& mods : Interfaces::Mods()) {
 				if (mods.second->getCategory() != Category::Ball)
-					return;
+					continue;
 				if (ImGui::MenuItem(mods.second->getName().c_str(),NULL,mods.second->enabled)) {
 					mods.second->Toggle();
-					if (mods.second->isEnabled()) {
-						mods.second->DrawMenu();
-					}
 				}
+
+			
+			}
 			ImGui::EndMenu();
-		}
 	}
 	if (ImGui::BeginMenu("Other Mods")) {
 			for (auto& mods : Interfaces::Mods()) {
@@ -50,6 +49,14 @@ void InGameGUI::Render() {
 		}
 		ImGui::EndMainMenuBar();
 	}
+
+	// Draws the actual windows outside of the menu loop cause DUH
+	for (auto& mods : Interfaces::Mods()) {
+		if (mods.second->isEnabled()) {
+			mods.second->DrawMenu();
+		}	
+	}
+
 }
 
 bool InGameGUI::MouseClickEvent(ClickEvent e, short x, short y) {
