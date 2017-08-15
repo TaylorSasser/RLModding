9680d12a1603902c97508cbb990960bec6e6dc9c
Scott Mitchell <mitchsco@sonoma.edu>
2017-08-14 17:16:18 -0700

    Added custom blog class, pls fix typing

---
+++ RLModding/Mods/Modules/Offline/CustomBlog.cpp
@@ -0,0 +1,50 @@
+#include "CustomBlog.h"
+
+CustomBlog::CustomBlog(std::string name, int key, Category category, GameState gamestate) : ModBase(name, key, category, gamestate) {}
+CustomBlog::CustomBlog(std::string name, int key) : ModBase(name, key) {}
+CustomBlog::~CustomBlog() {}
+
+void CustomBlog::onEnable() {
+	printf("Custom Blog enabled\n");
+}
+
+void CustomBlog::onDisable() {
+	printf("Custom blog disabled\n");
+}
+
+void CustomBlog::DrawMenu() {
+	ImGui::Begin("Custom Blog Menu", 0, ImVec2(400, 300), 0.75f);
+	ImGui::InputText("Title", title, IM_ARRAYSIZE(title));
+	ImGui::InputTextMultiline("Body", body, IM_ARRAYSIZE(body));
+	ImGui::InputText("Youtube URL", youtubeURL, IM_ARRAYSIZE(youtubeURL));
+	ImGui::InputText("Youtube Title", youtubeTitle, IM_ARRAYSIZE(youtubeTitle));
+	ImGui::InputText("MOTD", motd, IM_ARRAYSIZE(motd));
+	if (!bSet) {
+		if (ImGui::Button("Set")) {
+			bSet = true;
+		}
+	}
+	else {
+		if (ImGui::Button("Change")) {
+			bSet = false;
+		}
+	}
+
+	ImGui::End();
+		
+		
+}
+
+void CustomBlog::onMainMenuTick(Event* event) {
+	UGFxData_Community_TA* community = (UGFxData_Community_TA*)Utils::GetInstanceOf(UGFxData_Community_TA::StaticClass());
+	UOnlineGameBlog_X* blog = (UOnlineGameBlog_X*)Utils::GetInstanceOf(UOnlineGameBlog_X::StaticClass());
+
+	if (bSet && blog && community) {
+		blog->BlogTitle = Utils::to_fstring(title);
+		blog->BlogBody = Utils::to_fstring(body);
+		blog->YouTubeURL = Utils::to_fstring(youtubeURL);
+		blog->YouTubeTitle = Utils::to_fstring(youtubeTitle);
+		blog->MotD = Utils::to_fstring(motd);
+		community->UpdateBlogText();
+	}
+}
---
+++ RLModding/Mods/Modules/Offline/CustomBlog.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "../Utils/Utils.h"
+#include "../../ModBase.h"
+#include "../../../DrawManager/DrawManager.hpp"
+
+class CustomBlog : public ModBase
+{
+public:
+	CustomBlog(std::string name, int key, Category category, GameState gamestate);
+	CustomBlog(std::string name, int key);
+	~CustomBlog();
+
+	void onEnable() override;
+	void onDisable() override;
+	void DrawMenu() override;
+
+	void onMainMenuTick(Event*) override;
+
+private:
+	char title[128] = "Rocket Launcher";
+	char body[128] = "This is the body of the blog text";
+	char motd[128] = "Created by ButterandCream, Taylor and two.";
+	char youtubeTitle[128] = "Rocket League Mods";
+	char youtubeURL[128] = "rocketleaguemods.com";
+	bool bSet = false;
+};
