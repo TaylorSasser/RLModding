aa80bd28012fc7e16b0332f54e90e8e36967f9cf
Scott Mitchell <mitchsco@sonoma.edu>
2017-08-16 14:04:38 -0700

    Fixed getting steamid in player blacklist

--- RLModding/PlayerBlacklist/BlackList.cpp
+++ RLModding/PlayerBlacklist/BlackList.cpp
@@ -17,23 +17,17 @@ bool BlackList::Exists(std::string steam_id)
 
 //Check if thet are blacklisted
 void BlackList::Check() {
-	printf("Steam PlayerID: %d\n",GetPlayerId());
+	std::cout << "Steam ID: " << GetPlayerId() << std::endl;
 }
 
 //Get the user's steam id
-uint32_t BlackList::GetPlayerId() {
+__int64 BlackList::GetPlayerId() {
+	UOnlineSubsystemSteamworks* steam = reinterpret_cast<UOnlineSubsystemSteamworks*>(Utils::GetInstanceOf(UOnlineSubsystemSteamworks::StaticClass()));
+	if (steam) {
+		return steam->LoggedInPlayerId.Uid;
+	}
 }
 
-union SteamID_t {
-	struct SteamIDComponent_t {
-		uint32_t			m_unAccountID : 32;			// Account ID
-		unsigned int		m_unAccountInstance : 20;	// dynamic instance ID
-		unsigned int		m_EAccountType : 4;			// type of account - can't show as EAccountType, due to signed / unsigned difference
-		unsigned char		m_EUniverse : 8;			// universe this account belongs t
-	} m_comp;
-} m_steamid;
-
-//Static function to be able to check anywhere
 bool BlackList::isBlacklisted() {
 	return bBlacklisted;
 }
