e3bb1d6c861605df58a78de3a7c8d46f77ebaffd
TaylorSasser <Taylorsasser@gmail.com>
2017-08-19 20:53:16 -0400

    no message

--- RLModding/RL/SDK/RL_Basic.hpp
+++ RLModding/RL/SDK/RL_Basic.hpp
@@ -51,17 +51,15 @@ public:
 		return Data[i];
 	}
 
-	inline const T& GetByIndex(size_t i) const
-	{
+	inline const T& GetByIndex(size_t i) const {
 		return Data[i];
 	}
-	/* Not working. realloc can call on used memory regions and is not thread safe. mutexes?
 	inline void Add(T InputData) {
 		Data = (T*)realloc(Data, sizeof(T) * (Count + 1));
 		Data[Count++] = InputData;
 		Max = Count;
 	};
-	*/
+	
 
 private:
 	T* Data;
