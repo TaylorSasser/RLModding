1d8ae00b4b17ed4424045294aca02924e0e2b0d4
Scott Mitchell <mitchsco@sonoma.edu>
2017-10-01 18:48:56 -0700

    Finished Custom Blog

--- RLModding/Mods/Modules/Offline/CustomBlog.cpp
+++ RLModding/Mods/Modules/Offline/CustomBlog.cpp
@@ -5,25 +5,43 @@ CustomBlog::CustomBlog(std::string name, int key, Category category, GameState g
 CustomBlog::~CustomBlog() {}
 
 void CustomBlog::onEnable() {
-	bSet = true;
+
 }
 
 void CustomBlog::onDisable() {
-	bSet = false;
-	printf("Custom blog disabled\n");
 }
 
 void CustomBlog::DrawMenu() {
-	ImGui::Begin("Custom Blog Menu", &p_open, ImVec2(400, 300), 0.75f);
-	ImGui::InputText("Title", title, IM_ARRAYSIZE(title));
-	ImGui::InputTextMultiline("Body", body, IM_ARRAYSIZE(body));
-	ImGui::InputText("Link", URL, IM_ARRAYSIZE(URL));
-	ImGui::InputText("Image Url", ImageUrl, IM_ARRAYSIZE(ImageUrl));
+	ImGui::Begin("Custom Blog", &p_open, ImVec2(400, 300), 0.75f);
+	if (ImGui::CollapsingHeader("Carousel Tile Settings")) {
+		ImGui::InputText("Title", carousel_title, IM_ARRAYSIZE(carousel_title));
+		ImGui::InputText("Body", carousel_body, IM_ARRAYSIZE(carousel_body));
+		ImGui::InputText("Link", carousel_URL, IM_ARRAYSIZE(carousel_URL));
+		ImGui::InputText("Image Url", carousel_ImageUrl, IM_ARRAYSIZE(carousel_ImageUrl));
+	}
+	ImGui::Separator();
+	if (ImGui::CollapsingHeader("RLCS Tile Settings")) {
+		ImGui::InputText("Title", RLCS_title, IM_ARRAYSIZE(RLCS_title));
+		ImGui::InputText("Body", RLCS_body, IM_ARRAYSIZE(RLCS_body));
+		ImGui::InputText("Link", RLCS_URL, IM_ARRAYSIZE(RLCS_URL));
+		ImGui::InputText("Image Url", RLCS_ImageUrl, IM_ARRAYSIZE(RLCS_ImageUrl));
+	}
+	ImGui::Separator();
+	if (ImGui::CollapsingHeader("Community Tile Settings")) {
+		ImGui::InputText("Title", Comm_title, IM_ARRAYSIZE(Comm_title));
+		ImGui::InputText("Body", Comm_body, IM_ARRAYSIZE(Comm_body));
+		ImGui::InputText("Link", Comm_URL, IM_ARRAYSIZE(Comm_URL));
+		ImGui::InputText("Image Url", Comm_ImageUrl, IM_ARRAYSIZE(Comm_ImageUrl));
+	}
+	ImGui::Separator();
 	ImGui::InputText("MOTD", motd, IM_ARRAYSIZE(motd));
+	
 	if (ImGui::Button("Apply")) {
 		bSet = true;
 		printf("Applying cutsom blog options");
 	}
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("Note: Blog may take a while to update");
 	
 	if (!p_open) {
 		this->enabled = false;
@@ -43,16 +61,33 @@ void CustomBlog::onMainMenuTick(Event* event) {
 			TArray<class UBlogTile_X*> CarouselTiles = blog->CarouselTiles;
 			for (int i = 0; i < CarouselTiles.Num(); i++) {
 				if (CarouselTiles.IsValidIndex(i)) {
-					CarouselTiles[i]->Title = Utils::to_fstring(title);
-					CarouselTiles[i]->Description = Utils::to_fstring(body);
-					CarouselTiles[i]->WebURL = Utils::to_fstring(URL);
-					CarouselTiles[i]->ImageURL = Utils::to_fstring(ImageUrl);
+					CarouselTiles[i]->Title = Utils::to_fstring(carousel_title);
+					CarouselTiles[i]->Description = Utils::to_fstring(carousel_body);
+					CarouselTiles[i]->WebURL = Utils::to_fstring(carousel_URL);
+					CarouselTiles[i]->ImageURL = Utils::to_fstring(carousel_ImageUrl);
 				}
 			}
+			TArray<class UBlogTile_X*> RLCSTiles = blog->RLCSTiles;
+			for (int i = 0; i < RLCSTiles.Num(); i++) {
+				if (RLCSTiles.IsValidIndex(i)) {
+					RLCSTiles[i]->Title = Utils::to_fstring(RLCS_title);
+					RLCSTiles[i]->Description = Utils::to_fstring(RLCS_body);
+					RLCSTiles[i]->WebURL = Utils::to_fstring(RLCS_URL);
+					RLCSTiles[i]->ImageURL = Utils::to_fstring(RLCS_ImageUrl);
+				}
+			}
+			TArray<class UBlogTile_X*> CommunityTiles = blog->CommunityTiles;
+			for (int i = 0; i < CommunityTiles.Num(); i++) {
+				if (CommunityTiles.IsValidIndex(i)) {
+					CommunityTiles[i]->Title = Utils::to_fstring(Comm_title);
+					CommunityTiles[i]->Description = Utils::to_fstring(Comm_body);
+					CommunityTiles[i]->WebURL = Utils::to_fstring(Comm_URL);
+					CommunityTiles[i]->ImageURL = Utils::to_fstring(Comm_ImageUrl);
+				}
+			}
+			blog->MotD = Utils::to_fstring(motd);
 		}
-		
-		if (gfx)
-			gfx->UpdateBlog();
+		bSet = false;
 	}
 	
 	
