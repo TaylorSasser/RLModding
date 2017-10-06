b70784f50339bac94615fb7751acc566310e417d
Scott Mitchell <mitchsco@sonoma.edu>
2017-10-06 13:42:42 -0700

    OMG FULLSCREEN WORKS

--- RLModding/Hooks/DX9Hook.cpp
+++ RLModding/Hooks/DX9Hook.cpp
@@ -27,70 +27,87 @@ DWORD Device = NULL;
 
 static bool FirstRun = false;
 
-bool IsTopMost(HWND hwnd)
-{
-	WINDOWINFO info;
-	GetWindowInfo(hwnd, &info);
-	return (info.dwExStyle & WS_EX_TOPMOST) ? true : false;
-}
+void print_result(HRESULT result) {
+	switch (result) {
+	case D3D_OK:
+		printf("D3D_OK\n");
+		break;
+	case D3DERR_DEVICELOST:
+		printf("D3DERR_DEVICELOST\n");
+		break;
+	case D3DERR_INVALIDCALL:
+		printf("D3DERR_INVALIDCALL\n");
+		break;
+	case D3DERR_NOTAVAILABLE:
+		printf("D3DERR_NOTAVAILABLE\n");
+		break;
+	case D3DERR_OUTOFVIDEOMEMORY:
+		printf("D3DERR_OUTOFVIDEOMEMORY\n");
+		break;
 
-bool IsFullScreenSize(HWND hwnd, const int cx, const int cy)
-{
-	RECT r;
-	::GetWindowRect(hwnd, &r);
-	return r.right - r.left == cx && r.bottom - r.top == cy;
-}
+	default:
+		printf("something else...");
+		break;
 
-bool IsFullscreenAndMaximized(HWND hwnd)
-{
-	if (IsTopMost(hwnd))
-	{
-		const int cx = GetSystemMetrics(SM_CXSCREEN);
-		const int cy = GetSystemMetrics(SM_CYSCREEN);
-		if (IsFullScreenSize(hwnd, cx, cy))
-			return true;
 	}
-	return false;
 }
 
-BOOL CheckMaximized(HWND hwnd)
+// Unknown Cheats Code
+bool bCompare(const BYTE* pData, const BYTE* bMask, const char* szMask)
 {
-	if (IsFullscreenAndMaximized(hwnd))
-	{
-		return false; //there can be only one so quit here
-	}
-	return true;
+	for (; *szMask; ++szMask, ++pData, ++bMask)
+		if (*szMask == 'x' && *pData != *bMask)
+			return false;
+
+	return (*szMask) == NULL;
+}
+DWORD FindPattern(DWORD dwAddress, DWORD dwLen, BYTE *bMask, char * szMask)
+{
+	for (DWORD i = 0; i < dwLen; i++)
+		if (bCompare((BYTE*)(dwAddress + i), bMask, szMask))
+			return (DWORD)(dwAddress + i);
+
+	return 0;
 }
 
 
-void DX9Hook::InitGUI() {
+void HookAPI()
+{
+	DWORD EndSceneaddy;
+	DWORD* vtbl = 0;
+	DWORD table = FindPattern((DWORD)GetModuleHandle(TEXT("d3d9.dll")), 0x128000, (PBYTE)"\xC7\x06\x00\x00\x00\x00\x89\x86\x00\x00\x00\x00\x89\x86", "xx????xx????xx");
+	memcpy(&vtbl, (void*)(table + 2), 4);
+	EndSceneaddy = vtbl[42];
+	HookedReset = vtbl[16];
+	if (EndSceneaddy && HookedReset)
+	{
+		pD3D9EndScene = (HRESULT(WINAPI *)(LPDIRECT3DDEVICE9 pDevice))(DetourFunction((PBYTE)EndSceneaddy, (PBYTE)Hooked_EndScene));
+		pD3D9HookedReset = (D3D9HookedReset_t)DetourFunction((PBYTE)HookedReset, (PBYTE)Hooked_Reset);
+	}
+
+}
+
+// End UC code
+
+void DX9Hook::InitGUI() {	
 	HWND window = FindWindowA("LaunchUnrealUWindowsClient", "Rocket League (32-bit, DX9)");
 	WINDOWPLACEMENT p;
 	GetWindowPlacement(window, &p);
 	while (p.showCmd != 1) {
 		window = FindWindowA("LaunchUnrealUWindowsClient", "Rocket League (32-bit, DX9)");
 		GetWindowPlacement(window, &p);
-		if (p.showCmd == 1) {
-			break;
-		}
-		//printf("Showcmd: %d\n",p.showCmd);
 		Sleep(100);
 	}
 	printf("Window Placement: %d\n", p.showCmd);
 	Sleep(1000);
 
-	/*
-	bool isFullscreen = CheckMaximized(window);
-	if (GetWindowLong(window, GWL_STYLE) & WS_POPUP) {
-
-	}
-	std::cout << "fullscreen = " << isFullscreen << std::endl;
-	*/
+	// Works for fullscreen !!!
+	HookAPI();
 	
 
 	WNDCLASSEXA wc = { sizeof(WNDCLASSEX),CS_CLASSDC,D3D9MsgProc,0L,0L,GetModuleHandleA(NULL),NULL,NULL,NULL,NULL,"DX",NULL };
 	RegisterClassExA(&wc);
-	HWND hWnd = CreateWindowA("DX", NULL, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, GetDesktopWindow(), NULL, wc.hInstance, NULL);
+	HWND hWnd = CreateWindowA("DX", NULL, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, window, NULL, wc.hInstance, NULL);
 	this->Direct3DCreate9 = (myDirect3DCreate9)GetProcAddress(GetModuleHandle(TEXT("d3d9.dll")), "Direct3DCreate9");
 
 	LPDIRECT3D9 pD3D = Direct3DCreate9(32);
@@ -98,68 +115,47 @@ void DX9Hook::InitGUI() {
 
 	D3DPRESENT_PARAMETERS d3dpp;
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
-	d3dpp.Windowed = true; // setting this to true causes alt-tab bug for fullscreen
+	d3dpp.BackBufferCount = 1;
+	d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE;
+	d3dpp.MultiSampleQuality = 0;
+	d3dpp.hDeviceWindow = hWnd;
+	d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;
+	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;
+	d3dpp.Windowed = false; // setting this to true causes alt-tab bug for fullscreen
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	//d3dpp.hDeviceWindow = hWnd;
 	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.EnableAutoDepthStencil = FALSE;
 
 	IDirect3DDevice9 *pd3dDevice;
-	if (pd3dDevice)
-	{
-		HANDLE hHandle = GetModuleHandle(TEXT("d3d9.dll"));
-		
-		printf("Address of d3d9.dll: 0x%x\n", hHandle);	
+	HRESULT result = pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pd3dDevice);
+	printf("result = ");
+	print_result(result);
+	
+	/* Keep this as backup
+	DWORD* pVTable;
+	pVTable = (DWORD*)pd3dDevice;
 
-		HRESULT result = pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pd3dDevice);
-		printf("result = ");
-		switch (result) {
-		case D3D_OK:
-			printf("D3D_OK\n");
-			break;
-		case D3DERR_DEVICELOST:
-			printf("D3DERR_DEVICELOST\n");
-			break;
-		case D3DERR_INVALIDCALL:
-			printf("D3DERR_INVALIDCALL\n");
-			break;
-		case D3DERR_NOTAVAILABLE:
-			printf("D3DERR_NOTAVAILABLE\n");
-			break;
-		case D3DERR_OUTOFVIDEOMEMORY:
-			printf("D3DERR_OUTOFVIDEOMEMORY\n");
-			break;
+	if (pVTable) {
+		pVTable = (DWORD*)pVTable[0];
+		D3D9VTable = pVTable;
 
-		default:
-			printf("something else...");
-			break;
+		printf("pVTable = 0x%x", pVTable);
 
-		}
+		if (hWnd != NULL)
+			DestroyWindow(hWnd);
 
-		printf("Address of pd3dDevice: 0x%x\n", pd3dDevice);
-		printf("pd3dDevice - d3d9.dll address: 0x%x\n", pd3dDevice - hHandle);
-		DWORD* pVTable = (DWORD*)pd3dDevice;
+		EndScene = D3D9VTable[42];
+		HookedReset = D3D9VTable[16];
 
-		if (pVTable) {
-			pVTable = (DWORD*)pVTable[0];
-			D3D9VTable = pVTable;
-
-			printf("pVTable = 0x%x", pVTable);
-
-			if (hWnd != NULL)
-				DestroyWindow(hWnd);
-
-			EndScene = D3D9VTable[42];
-			HookedReset = D3D9VTable[16];
-
-			pD3D9EndScene = (D3D9EndScene_t)DetourFunction((PBYTE)EndScene, (PBYTE)Hooked_EndScene);
-			pD3D9HookedReset = (D3D9HookedReset_t)DetourFunction((PBYTE)HookedReset, (PBYTE)Hooked_Reset);
-		}
-		
+		pD3D9EndScene = (D3D9EndScene_t)DetourFunction((PBYTE)EndScene, (PBYTE)Hooked_EndScene);
+		pD3D9HookedReset = (D3D9HookedReset_t)DetourFunction((PBYTE)HookedReset, (PBYTE)Hooked_Reset);
 	}
+	*/
+	
 		
 	
-
 }
+
 
 void DX9Hook::RemoveHook() {
 	DetourRemove((PBYTE)pD3D9EndScene, (PBYTE)Hooked_EndScene);
