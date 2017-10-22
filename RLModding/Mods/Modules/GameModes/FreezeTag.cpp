570e82ceb2dda08ab5a027336dd402839378e76b
Scott Mitchell <mitchsco@sonoma.edu>
2017-10-21 23:47:37 -0700

    Fixed FreezeTag

--- RLModding/Mods/Modules/GameModes/FreezeTag.cpp
+++ RLModding/Mods/Modules/GameModes/FreezeTag.cpp
@@ -61,16 +61,19 @@ void FreezeTag::OnRoundStart(Event* e) {
 				if (tempController->IsA(SDK::AAIController_TA::StaticClass())) {
 					AAIController_TA* currController = (AAIController_TA*)tempController;
 					_IT = currController->Car;
+					bSetWhosIt = true;
 				}
 				else if (tempController->IsA(SDK::APlayerController_TA::StaticClass())) {
 					APlayerController_TA* currController = (APlayerController_TA*)tempController;
-					_IT = currController->Car;	
+					_IT = currController->Car;
+					bSetWhosIt = true;
 				}
 				std::cout << "Set _IT\n";
 				_IT_Name = _IT->PlayerReplicationInfo->PlayerName.ToString();
 				_IT->SetMaxLinearSpeed(_IT->MaxLinearSpeed * 2); // Can go twice as fast
+				localGameEvent->SetTotalGameBalls(0);
 				Utils::BroadcastMessage(localGameEvent, _IT_Name + " is IT!");
-				bSetWhosIt = true;
+				
 				
 			}
 		}
@@ -87,22 +90,22 @@ void FreezeTag::onCarSpawned(Event* e) {
 }
 
 void FreezeTag::onCarDemolished(Event* e) {
-	if (bStarted) {
-
-	}
 }
 
 void FreezeTag::OnCarBumped(Event* e) {
 	if (bStarted) {
-		ACar_TA *HitCar = (SDK::ACar_TA*)e->getCallingObject();
+		ACar_TA *HitCar = reinterpret_cast<SDK::ACar_TA*>(e->getCallingObject());
 
 		ACar_TA *Car = reinterpret_cast<SDK::ACar_TA*>(e->getParams<ACar_TA_EventBumpedCar_Params>()->Car);
 		//ACar_TA *HitCar = reinterpret_cast<SDK::ACar_TA*>(e->getParams<ACar_TA_EventBumpedCar_Params>()->HitCar);
 		// cant compare cars, _IT == HitCar doesnt work
 		if (Car != nullptr && HitCar != nullptr && _IT == HitCar) {
 			std::cout << "Hit!\n";
-			Car->bPodiumMode = true;
-			if (Car->bPodiumMode) numTagged++;
+			if (!Car->bPodiumMode) {
+				Car->bPodiumMode = true;
+				numTagged++;
+			}
+			
 		}
 		AGameEvent_Soccar_TA* localGameEvent = reinterpret_cast<AGameEvent_Soccar_TA*>(InstanceStorage::GameEvent());
 		if (localGameEvent) {
@@ -114,6 +117,7 @@ void FreezeTag::OnCarBumped(Event* e) {
 				localGameEvent->StartNewRound();
 				bSetWhosIt = false;
 				numTagged = 0;
+				std::cout << _IT_Name << " Won!\n";
 
 			}
 		}
