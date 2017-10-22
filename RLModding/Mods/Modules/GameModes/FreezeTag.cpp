e3c22e11470ccf86eea01e0dfa3bd6319b7cd00e
Scott Mitchell <mitchsco@sonoma.edu>
2017-10-21 19:03:40 -0700

    Updated FreezeTag

--- RLModding/Mods/Modules/GameModes/FreezeTag.cpp
+++ RLModding/Mods/Modules/GameModes/FreezeTag.cpp
@@ -67,11 +67,13 @@ void FreezeTag::OnRoundStart(Event* e) {
 			TArray< class ATeam_TA* > gameTeams = localGameEvent->Teams;
 			if (localGameEvent->Teams.IsValidIndex(0)) {
 				int team_idx = rand() % gameTeams.Num();
+				std::cout << gameTeams.Num() << std::endl;
 				if (gameTeams.IsValidIndex(team_idx)) {
 					int player_idx = rand() % gameTeams[team_idx]->Members.Num();
 					TArray<class APRI_TA*> players = gameTeams[team_idx]->Members;
 					if (players.IsValidIndex(player_idx)) {
 						_IT = gameTeams[team_idx]->Members[player_idx]->Car;
+						_IT->SetMaxLinearSpeed(_IT->MaxLinearSpeed * 2); // Can go twice as fast
 						Utils::BroadcastMessage(localGameEvent, _IT->PlayerReplicationInfo->PlayerName.ToString() + " is IT!");
 						bSetWhosIt = true;
 					}
@@ -83,7 +85,6 @@ void FreezeTag::OnRoundStart(Event* e) {
 
 void FreezeTag::onPlayerTick(Event* event) {
 	if (bStarted) {
-
 	}
 }
 
@@ -101,11 +102,31 @@ void FreezeTag::OnCarBumped(Event* e) {
 	if (bStarted) {
 		ACar_TA *Car = reinterpret_cast<SDK::ACar_TA*>(e->getParams<ACar_TA_EventBumpedCar_Params>()->Car);
 		ACar_TA *HitCar = reinterpret_cast<SDK::ACar_TA*>(e->getParams<ACar_TA_EventBumpedCar_Params>()->HitCar);
-		// Not working, cant compare cars
-		if (Car != nullptr && HitCar != nullptr) {
+		printf("HitCar: 0x%x\n", HitCar);
+		printf("_IT: 0x%x\n", _IT);
+		printf("_IT - HitCar: 0x%x\n", _IT - HitCar);
+		// cant compare cars, _IT == HitCar doesnt work
+		if (Car != nullptr && HitCar != nullptr && HitCar == _IT) {
 			std::cout << "Hit!\n";
 			Car->bPodiumMode = !Car->bPodiumMode;
 			Car->bPodiumMode ? numTagged++ : numTagged--;
+		}
+		AGameEvent_Soccar_TA* localGameEvent = reinterpret_cast<AGameEvent_Soccar_TA*>(InstanceStorage::GameEvent());
+		if (localGameEvent) {
+			int total = 0;
+			if (localGameEvent->Teams.IsValidIndex(0)) {
+				total += localGameEvent->Teams[0]->Members.Num() - 1;
+			}
+			if (localGameEvent->Teams.IsValidIndex(1)) {
+				total += localGameEvent->Teams[1]->Members.Num() - 1;
+			}
+			if (numTagged == total - 1) {
+				//Win!
+				Utils::BroadcastMessage(localGameEvent, _IT->PlayerReplicationInfo->PlayerName.ToString() + " Wins!");
+				localGameEvent->StartNewRound();
+				bSetWhosIt = false;
+
+			}
 		}
 		
 		
