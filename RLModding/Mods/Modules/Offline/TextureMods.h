039be04f2e5b65670467309aa2f224b793932f86
GlenHumphrey <glen_humphrey@hotmail.com>
2017-10-23 17:30:24 +0100

    TextureMods - When a skin is applied, the parameters are autofilled. GUI update.

--- RLModding/Mods/Modules/Offline/TextureMods.h
+++ RLModding/Mods/Modules/Offline/TextureMods.h
@@ -13,7 +13,7 @@ public:
 	void onMenuClose() override;
 	void DrawMenu() override;
 	void onMainMenuTick(Event*) override;
-	
+
 	//Decal Mods
 	SDK::UMaterialInstanceConstant* TextureMods::GetCurrentSkinName();
 	void TextureMods::UpdateDecalTextures();
@@ -30,13 +30,35 @@ public:
 	//Tools
 	SDK::UTexture2D* DownloadTexture(char* stringURL);
 	void TextureMods::ReplaceTexture(char* TextureName, char* NewTextureURL);
+	void TextureMods::AddNewReplacementTexture();
+	void TextureMods::RemoveReplacementTexture();
 	void TextureMods::RefreshCar();
 
+	void TextureMods::PopulateParams();
+	void TextureMods::PopulateTextureParameters(UMaterialInstanceConstant* skinMAT);
+	void TextureMods::PopulateScalarParameters(UMaterialInstanceConstant* skinMAT);
+	void TextureMods::PopulateVectorParameters(UMaterialInstanceConstant* skinMAT);
+
+	void TextureMods::SetTextureValue();
+	void TextureMods::SetScalarValue();
+	void TextureMods::SetVectorValue();
+
+	void TextureMods::ReplaceBallTexture();
+
 private:
-	int tickCounter = 0;
-	bool bReplaceLoopers = false;
-	char ACWheelsURL[2056] = "https://i.imgur.com/SvEZpGf.png";
+	SDK::UHttpRequestInterface* anyRequestInterface = NULL;
+	UMaterialInstanceConstant* skinMaterials = NULL;
 	bool p_open = true;
+
+	int tickCounter = 0;
+	//AC Wheels
+	bool bReplaceLoopers = false;
+	char ACWheelsURL[256] = "https://i.imgur.com/SvEZpGf.png";
+
+	//Ball Texture
+	bool bReplaceBallTexture = false;
+	char BallTextureURL[256] = "";
+	char* ballDiffuse = "Ball_Default_Textures.Textures.Ball_Default00_D";
 
 	//Custom Skin URLs
 	char curvaturePackURL[2056] = "";
@@ -45,10 +67,49 @@ private:
 	bool bUpdateDecalTextures = false;
 
 	//uMod Replacement
-	char textureToReplace[2056] = "Skin_Octane_Flames.Pepe_Flames";
-	char newReplacementTexture[2056] = "";
+	struct TextureReplacement {
+		char textureName[256] = "";
+		char url[256] = "";
+	};
+
+	char* textureToReplace = "Skin_Octane_Flames.Pepe_Flames";
+	char* newReplacementTextureURL = "";
+	TextureReplacement temp = { "Skin_Octane_Flames.Pepe_Flames", "" };
+	std::vector<TextureReplacement>textureReplacements = { temp };
+	bool bAddNewReplacementTexture = false;
+	bool bRemoveReplacementTexture = false;
+	int replacementTextureToRemoveIndex = 0;
 	bool bReplaceTextures = false;
 
 	//Reset
 	bool bResetTextures = false;
+
+	/////////////
+	//Skin Mods//
+	/////////////
+	bool bPopulateParams = false;
+
+	//Texture
+	struct Buffer {
+		char text[256];
+	};
+	std::vector<const char*>textureNames = {};
+	std::vector<Buffer>textureBuffers = {};
+	int textureNameIndex = 0;
+	char* textureValue = "";
+	bool bSetTextureValue = false;
+
+	//Scalar
+	std::vector<const char*>scalarNames = {};
+	std::vector<float>scalarValues = {};
+	int scalarNameIndex = 0;
+	int scalarValue = 0;
+	bool bSetScalarValue = false;
+
+	//Vector
+	std::vector<const char*>vectorNames = {};
+	std::vector<FLinearColor>vectorValues = {};
+	int vectorNameIndex = 0;
+	FLinearColor vectorValue = { 0,0,0,0 };
+	bool bSetVectorValue = false;	
 };
