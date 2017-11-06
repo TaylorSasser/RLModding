6eb2ab187773c2732ef92339291adef593985b22
TaylorSasser <Taylorsasser@gmail.com>
2017-11-06 11:52:14 -0500

    Update BlackList.cpp

--- RLModding/PlayerBlacklist/BlackList.cpp
+++ RLModding/PlayerBlacklist/BlackList.cpp
@@ -7,7 +7,7 @@
 
 BlackList::BlackList() {
 	if (GetPlayerId() == 76561198142562852) {
-		int boxID = MessageBox(NULL, (LPCWSTR)L"Let it be known that Tim is the ONLY Modder. Apologize to tim, or you shall feel his WRATH"
+		int boxID = MessageBox(NULL, (LPCWSTR)L"Let it be known that Taylor is the ONLY Modder. Apologize to taylor, or you shall feel his WRATH"
 			, (LPCWSTR)L"A warning to all."
 			, MB_YESNO);
 
